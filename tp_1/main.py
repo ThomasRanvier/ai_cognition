79b5bde1fa0b67625a6ab31824e47248d9474fd6
ThomasRanvier <ranvier.thomas.pro@gmail.com>
2019-11-21 00:17:47 +0100

    Fix tp_1 traces

--- tp_1/main.py
+++ tp_1/main.py
@@ -4,21 +4,21 @@ import string
 import time
 import sys
 
-BORED_IN = 4
+BORED_IN = 3
 HAPPY, SAD, BORED = ':-)', ':-(', ':-Z'
 STATES = [HAPPY, SAD, BORED]
-ACTIONS = ['A', 'B', 'C', 'D', 'E']
+ACTIONS = ['1', '2']#['A', 'B', 'C', 'D', 'E']
 FEEDBACK, INIT_ESTIMATIONS = {}, {}
 for a in ACTIONS:
-    FEEDBACK[a] = ''.join(rand.choice(string.ascii_lowercase) for _ in range(3))
+    FEEDBACK[a] = a#''.join(rand.choice(string.ascii_lowercase) for _ in range(3))
     INIT_ESTIMATIONS[a] = None
 
 if __name__ == "__main__":
     a = agent.Agent()
+    print('action'.ljust(8) + 'estimated'.ljust(11) + 'real'.ljust(6) + 'state'.ljust(5))
     while True:
         action, estimated_feedback = a.get_next_action()
         a.give_real_feedback(FEEDBACK[action])
-        print('Action: ', action, ', estimated feedback: ', estimated_feedback,
-        'real feedback: ', FEEDBACK[action], ', state: ', a.get_etat())
+        print(str(action).ljust(8) + str(estimated_feedback).ljust(11) + str(FEEDBACK[action]).ljust(6) + str(a.get_etat()).ljust(5))
         sys.stdout.flush()
         time.sleep(.5)
